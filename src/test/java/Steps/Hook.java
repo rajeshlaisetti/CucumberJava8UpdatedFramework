178b088e8ad601eb118f4572ecf9f08d7b0d0966
Rajesh Laisetti <rajeshvarmalaisetti@gmail.com>
2019-01-21 16:14:18 +0530

    updated hook

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -8,8 +8,7 @@ public class Hook {
     WebDriver driver;
     @Before
     public WebDriver getChromeBrowser() {
-        System.setProperty("webdriver.chrome.driver", "C:\\SavedFacets\\SavedFacetBranch\\CucumberDemo\\src\\drivers\\chromedriver.exe");
-       // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/drivers/chromedriver.exe");
         driver = new ChromeDriver();
         return driver;
     }
