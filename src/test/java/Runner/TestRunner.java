223a6e94e2050a698995746ecd0fff31f94e77f4
rajeshlaisetti <rajeshvarmalaisetti@gmail.com>
2019-01-21 10:44:29 +0530

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -3,7 +3,6 @@ package Runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
          strict = true
